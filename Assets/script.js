a882ffc378dad2a3f0965be50e1fe963fd609a01
stephdanid <stephaniedanidominguez@gmail.com>
2020-11-04 23:08:42 -0600

    created more objects in questions array, added pseudocode comments to show my though proccesses, began function to set question, added more content to readme.md, created more variables to hook in html

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,59 @@
+// questions array that we display on our hidden html after click event
 let quizQuestions = [{
-    q: "What is not a primitive data types?",
-    answers: ["strings", "booleans", "alerts", "undefined"],
-    correct: "alerts"
-}]
-
+            q1: "What is not a primitive data types?",
+            answers: ["strings", "booleans", "alerts", "undefined"],
+            correct: "alerts"
+        },
+        {
+            q2: "What is an example of a Javascript Object?",
+            answers: ["let cat = {animal:'cat', type:'orange tabby', name:'Nibble'}",
+                "let cat = 'Nibble'", "let nibble = true", "let catAge = 2"
+            ],
+            correct: "let cat = {animal:'cat', type:'orange tabby', name:'Nibble'}"
+        }
+    ]
+    //html on page load
 let startbtn = document.querySelector("#startbtn");
 let startHtmldiv = document.querySelector(".startHtml");
+// variables to our questions div and buttons
 let questionsdiv = document.querySelector(".questionsdiv");
+let questionsDivH1 = document.querySelector(".qDisplay")
+let choice1Btn = document.querySelector("#choice1");
+let choice2Btn = document.querySelector("#choice2");
+let choice3Btn = document.querySelector("#choice3");
+let choice4Btn = document.querySelector("#choice4");
 
+// this is our onclick event to show our questions div and setting our timer after click and to display our 1st question and answers
 startbtn.addEventListener("click", function() {
     startHtmldiv.style.display = "none";
     questionsdiv.style.display = "block";
     setTime()
+    setQuestion()
 })
+
+// variable to begin our timer with and hooking into our h3 class of timer to put our text timer in
+let timerH3 = document.querySelector(".timer")
 let Timeleft = 60
 
+// function to decrement our timer/clear our timer/get our timer on screen
 function setTime() {
     const timerInterval = setInterval(function() {
-        console.log(Timeleft)
+
         Timeleft--
+
+        timerH3.textContent = Timeleft;
+
         if (Timeleft === 0) {
             clearInterval(timerInterval)
         }
     }, 1000)
+}
+
+// now for our function to to display our question
+// okay pscode I want this function to grab my global function to access my h1 on my index.html
+// to display the question and then add my event listener to my buttons to also disply my  
+function setQuestion() {
+    questionsDivH1.textContent = quizQuestions
+
+
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+This my project for creating a code equipped with a timer and button to start the quiz. Each questions will have variety of answers. it will lopp through questions, and detail the correct answer while deducting time if your answer is wrong. 
+here is the link to the deployed application
+
+https://stephdanid.github.io/codeQuiz/
